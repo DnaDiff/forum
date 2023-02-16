66ebcc0c46ac95da4a4774dd3e74171f5ad72c90
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-02-16 17:16:02 +0100

    support for sign in

--- back-end/JS/header.js
+++ back-end/JS/header.js
@@ -7,19 +7,42 @@ function loadHeader() {
       document.querySelector("#header-placeholder").innerHTML = header;
       // load the signup script
       loadSignUp();
+      loadSignIn();
     });
 }
 
 function loadSignUp() {
+
+  
+  const dimmer = document.querySelector("#dim");
   const signupBtn = document.querySelector("#signup-btn");
   const signupFormModal = document.querySelector("#signup-form-modal");
   const closeBtn = document.querySelector(".close-btn");
 
   signupBtn.addEventListener("click", function () {
     signupFormModal.style.display = "block";
+    dimmer.style.display = "block";
   });
 
   closeBtn.addEventListener("click", function () {
     signupFormModal.style.display = "none";
+    dimmer.style.display = "none";
+  });
+}
+// Reusing the same code for the sign up modal because of problems with the sign in modal not working properly when the code is not separated
+function loadSignIn() {
+  const dimmer = document.querySelector("#dim");
+  const signinBtn = document.querySelector("#signin-btn");
+  const signinFormModal = document.querySelector("#signin-form-modal");
+  const closeBtn = document.querySelector(".close-btn1");
+
+  signinBtn.addEventListener("click", function () {
+    signinFormModal.style.display = "block";
+    dimmer.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+    signinFormModal.style.display = "none";
+    dimmer.style.display = "none";
   });
 }
