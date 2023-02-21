e436547423700b9704f796b3ca202acc706f1f10
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-02-21 14:19:01 +0200

    integrated modals into header

--- back-end/JS/header.js
+++ back-end/JS/header.js
@@ -8,12 +8,12 @@ function loadHeader() {
       // load the signup script
       loadSignUp();
       loadSignIn();
+      loadPost();
+      loadComment();
     });
 }
 
 function loadSignUp() {
-
-  
   const dimmer = document.querySelector("#dim");
   const signupBtn = document.querySelector("#signup-btn");
   const signupFormModal = document.querySelector("#signup-form-modal");
@@ -45,4 +45,48 @@ function loadSignIn() {
     signinFormModal.style.display = "none";
     dimmer.style.display = "none";
   });
+}
+
+function loadPost() {
+  const dimmer = document.querySelector("#dim");
+  const postBtn = document.querySelector("#post-btn");
+  const postFormModal = document.querySelector("#post-form-modal");
+  const closeBtn = document.querySelector(".close-btn2");
+
+  document.getElementById("post").addEventListener("input", updateCharCount);
+
+  postBtn.addEventListener("click", function () {
+    postFormModal.style.display = "block";
+    dimmer.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+    postFormModal.style.display = "none";
+    dimmer.style.display = "none";
+  });
+}
+
+function loadComment() {
+  const dimmer = document.querySelector("#dim");
+  const commentBtn = document.querySelector("#comment-btn");
+  const commentFormModal = document.querySelector("#comment-form-modal");
+  const closeBtn = document.querySelector(".close-btn3");
+
+  commentBtn.addEventListener("click", function () {
+    commentFormModal.style.display = "block";
+    dimmer.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+    commentFormModal.style.display = "none";
+    dimmer.style.display = "none";
+  });
+}
+
+function updateCharCount() {
+  var content = document.getElementById("post");
+  var charsLeft = document.getElementById("charsLeft");
+  var maxChars = content.getAttribute("maxlength");
+  var remainingChars = maxChars - content.value.length;
+  charsLeft.innerHTML = remainingChars;
 }
