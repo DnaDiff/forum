66dad6664f13e42f250e9e22a0b8d10f491d21cc
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-02-22 14:47:04 +0200

    format and database

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	startServer()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./front-end/templates/home.html")
+	})
+	// Serve CSS files
+	cssHandler := http.StripPrefix("/css/", http.FileServer(http.Dir("./front-end/css/")))
+	http.Handle("/css/", cssHandler)
+
+	// Serve JS files
+	jsHandler := http.StripPrefix("/js/", http.FileServer(http.Dir("./back-end/JS/")))
+	http.Handle("/js/", jsHandler)
+
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
