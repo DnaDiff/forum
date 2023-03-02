388815f21ca91daf55d144189d694ee21e510d91
Kevazy <25287241+Kevazy@users.noreply.github.com>
2023-03-02 16:56:57 +0200

    WIP login
    Co-authored-by: J≡r≡ <Jeresss@users.noreply.github.com>

---
+++ src/handle_login.go
@@ -0,0 +1,24 @@
+package forum
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
+
+// var store = sessions.NewCookieStore([]byte("my_secret_key"))
+
+func HandleLogin(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println(username, password)
+}
