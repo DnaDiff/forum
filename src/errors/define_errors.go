4eff7b5a4b3fda963d6a1eb55d3f365f8cd8c1ac
Kevazy <25287241+Kevazy@users.noreply.github.com>
2023-03-03 15:47:34 +0200

    Further error improvements
    Co-authored-by: J≡r≡ <Jeresss@users.noreply.github.com>

--- src/errors/define_errors.go
+++ src/errors/define_errors.go
@@ -6,10 +6,7 @@ type Error struct {
 	Original error
 }
 
+// Custom error messages for specific error cases
 const (
-	INTERNAL_SERVER_ERROR    = "500 Internal Server Error"
 	TEMPLATE_CORRUPTED_ERROR = "500 Internal Server Error: Template Corrupted"
-	NOT_FOUND_ERROR          = "404 Not Found"
-	BAD_REQUEST_ERROR        = "400 Bad Request"
-	UNAUTHROIZED_ERROR       = "401 Unauthorized"
 )
--- src/errors/error.go
+++ src/errors/error.go
@@ -8,12 +8,20 @@ import (
 )
 
 func HttpError(w http.ResponseWriter, err Error) {
-	if err.Code > 0 && err.Message == "" {
-		err.Message = http.StatusText(err.Code)
-	} else if err.Code == 0 && err.Message == "" {
+	// If the error code is not set, set it to 500
+	if err.Code == 0 {
 		err.Code = http.StatusInternalServerError
+	}
+	// If the error message is not set, set it to the default message for the error code
+	if err.Message == "" {
 		err.Message = http.StatusText(err.Code)
 	}
+	// If the original error is not set, set it to the error message
+	if err.Original == nil {
+		err.Original = fmt.Errorf(err.Message)
+	}
+
+	// Print the error to the console and send the error to the client
 	fmt.Fprintln(os.Stderr, err.Original)
 	handleError(w, &err)
 }
