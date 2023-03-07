d2a4de7d8d1863ea34af02b737d95aeb2b0b44a7
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-03-07 13:57:23 +0200

    testing the new functions

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/DnaDiff/forum/src/database"
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/DnaDiff/forum/src/handlers"
 )
@@ -21,16 +23,35 @@ func main() {
 	}
 	defer db.Close()
 
+	database.InitDatabase(db)
 	// Execute the contents of the init.sql file
-	sqlScript, err := ioutil.ReadFile("./database/init.sql")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// sqlScript, err := ioutil.ReadFile("./database/init.sql")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	_, err = db.Exec(string(sqlScript))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// _, err = db.Exec(string(sqlScript))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// Add a user to the database
+	// database.AddUser(db)
+
+	// Get a user from the database
+	// user, err := database.GetUser(db)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// userInfo, err := json.Marshal(user)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(string(userInfo) + "\n" + "User read successfully.")
 
 	// Create the mux to handle the routes
 	mux := handlers.RouteHandler(db)
