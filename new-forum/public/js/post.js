35f9e37adbaad9ba2fa19960a9abfb5e015a0409
Kevazy <25287241+Kevazy@users.noreply.github.com>
2023-03-09 20:24:53 +0200

    WIP post template

---
+++ new-forum/public/js/post.js
@@ -0,0 +1,31 @@
+function appendNewPost(category, title, content, author, date, avatarUrl) {
+  // create a new post element
+  const postElement = document.createElement("div");
+  postElement.classList.add("post");
+  postElement.innerHTML = `
+    <div class="post-avatar">
+      <img src="${"https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"}" alt="${author}'s avatar">
+    </div>
+    <div class="post-content">
+      <h2>${title}</h2>
+      <p class="content">${content}</p>
+      <p class="author">Started by ${author} on ${date}</p>
+    </div>
+  `;
+
+  // select the container element
+  const categoryElement = document.getElementById(category);
+
+  // append the new post element to the container element
+  categoryElement.appendChild(postElement);
+}
+
+for (let i = 0; i < 10; i++) {
+  appendNewPost(
+    "post-container",
+    "New Post",
+    "This is a new post",
+    "John Doe" + i,
+    "2020-01-01"
+  );
+}
