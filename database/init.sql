a1896879449e2c56f3c70993477665de8fdfb1b2
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-03-10 16:49:55 +0200

    remake for like and dislike

--- database/init.sql
+++ database/init.sql
@@ -32,3 +32,12 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (post_id) REFERENCES posts(id)
 );
+CREATE TABLE IF NOT EXISTS likes (
+    id INTEGER NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER,
+    comment_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+);
