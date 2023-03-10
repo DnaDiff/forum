1fa85c180db379912317aff54ae2990913a8ebf9
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-03-10 20:25:16 +0200

    added missing dislike table

--- database/init.sql
+++ database/init.sql
@@ -46,3 +46,12 @@ CREATE TABLE IF NOT EXISTS likes (
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (comment_id) REFERENCES comments(id)
 );
+CREATE TABLE IF NOT EXISTS dislikes (
+    id INTEGER NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER,
+    comment_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+);
