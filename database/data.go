1c316d9fd56085c2eb7d91a818579e6bddb324ec
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-03-16 16:34:31 +0200

    possible structure

---
+++ database/data.go
@@ -0,0 +1,33 @@
+package database
+
+
+
+func setupDatabase() {
+	// Connect to database
+	Connect()
+	// Create tables
+	CreateTables()
+	// Create admin user
+	CreateAdmin()
+	// Create default categories
+	CreateCategories()
+}
+
+func Connect() {
+	// Connect to database
+}
+
+func CreateTables() {
+
+	// create the tables for the users and posts
+	
+
+}
+
+func CreateAdmin() {
+	// Create admin user
+}
+
+func CreateCategories() {
+	// Create default categories
+}
