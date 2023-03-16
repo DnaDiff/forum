544d96712a1c18dac2ad62167a76e01bf4db5741
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-03-16 16:35:50 +0200

    working post functions

--- src/database/post.go
+++ src/database/post.go
@@ -6,15 +6,12 @@ import (
 )
 
 type Post struct {
-	ID           int
-	UserID       int
-	Title        string
-	Content      string
-	Category     string
-	Created      time.Time
-	LikeCount    int
-	DislikeCount int
-	CommentCount int
+	ID       int
+	UserID   int
+	Title    string
+	Content  string
+	Category string
+	Created  time.Time
 }
 
 // CreatePost creates a post and updates the post count for the user who created the post
@@ -39,23 +36,12 @@ func CreatePost(db *sql.DB, post *Post) error {
 	}
 	post.ID = int(postId)
 
-	// Update the user's post count
-	stmt, err = db.Prepare("UPDATE users SET post_count = post_count + 1 WHERE id = ?")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(post.UserID)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
+
 // RemovePost removes a post and updates the post count for the user who created the post
-func removePost(db *sql.DB, postId int) error {
+func RemovePost(db *sql.DB, postId int) error {
 	tx, err := db.Begin()
 	if err != nil {
 		return err
@@ -80,13 +66,6 @@ func removePost(db *sql.DB, postId int) error {
 		return err
 	}
 
-	// Remove all likes associated with the post using RemoveLikePost
-
-	// _, err = stmt.Exec(postId)
-	// if err != nil {
-	//     return err
-	// }
-
 	// Remove the post itself
 	stmt, err = tx.Prepare("DELETE FROM posts WHERE id = ?")
 	if err != nil {
@@ -98,18 +77,5 @@ func removePost(db *sql.DB, postId int) error {
 	if err != nil {
 		return err
 	}
-
-	// Update the user's post count
-	stmt, err = tx.Prepare("UPDATE users SET post_count = post_count - 1 WHERE id = (SELECT user_id FROM posts WHERE id = ?)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(postId)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
